13fd0c6dff225c7f57accdd475eb3bbe5b0aa1bb
Nikko Valencia <nikko_129@hotmail.com>
2021-04-30 23:15:45 -0700

    Assignment2

---
+++ NikkoValencia_Assignment_2/Assets/Assignment2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float dogCooldown = 2.0f;
+    public float dogTimer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // prevents spamming the spacebar
+        dogTimer += Time.deltaTime;
+        if(dogTimer < dogCooldown)
+        {
+            return;
+        }
+
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            dogTimer = 0;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
---
+++ Prototype3/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+    private Vector3 spawnPos = new Vector3(30.0f, 0.0f, 0.0f);
+    private float startDelay = 2.0f;
+    private float repeatRate = 2.0f;
+    private PlayerController playerControllerScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+   private void SpawnObstacle()
+    {
+        if(playerControllerScript.gameOver == false)
+        {
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        }
+    }
+}
